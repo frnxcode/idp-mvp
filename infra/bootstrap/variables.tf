e0129d5b47c7c688c17ebb375a60187fb36a4339
frnxcode <olukayode-gomes@hotmail.com>
2026-05-20 15:08:29 +0100

    feat: add terraform bootstrap for remote state

---
+++ infra/bootstrap/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "eu-west-1"
+}
+
+variable "state_bucket_name" {
+  default = "idp-mvp-tfstate-684779207098"
+}
+
+variable "lock_table_name" {
+  default = "idp-mvp-tfstate-lock"
+}
